5f322e54061ae24254a73f4530ed1da1e7b2e071
Nikita <43483640+Si-ja@users.noreply.github.com>
2020-07-22 20:21:42 +0200

    Original Fix

--- README.md
+++ README.md
@@ -1,68 +1,11 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# "Drink Some Water"
 
-## Available Scripts
+"Drink Some Water" is a web browser IDLE clicker game, just as many that already exist (e.g. Cookie Clicker). The point of this project was to get a bit familiarized with the `JavaScript`, particularly `React.js` and refresh own knowledge and acquire new one on HTML and CSS.
 
-In the project directory, you can run:
+## Results:
 
-### `npm start`
+I consider it to be successful. A game runs in a browser, where a user can either click on the main `Bottle` on the screen to produce water bottles. Or buy upgrades that will do it for them. Currently the game has only 3 upgrades and is fully built for fun. No ads, no more than 1 web page, no plans to expand so far.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+I also wanted to make graphics to something visual and as I enjoy pixel art - this is the style I chose to approach the task with.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+## Example:
--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import bottle_image from "./graphics/bottle.png";
+import glass_upgrade from "./graphics/glass.png";
+import bucket_upgrade from "./graphics/bucket.png";
+import well_upgrade from "./graphics/well.png";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+var consumption_rate = 0;
+var consumed_total = 0;
+
+var glass_price = 10;
+var bucket_price = 200;
+var well_price = 1000;
+
+var glass_owned = 0;
+var bucket_owned = 0;
+var well_owned = 0;
+
+function sip() {
+  consumed_total += 1;
+  var consumednew = <h3 id="WaterBottlesCount">Water Bottles Produced: {consumed_total.toFixed(0)}</h3>
+  ReactDOM.render(consumednew, document.getElementById("counter"))
+}
+
+function new_glass() {
+  if (consumed_total >= glass_price) {
+    glass_owned += 1;
+    var temp_owned = "OWNED: " + glass_owned;
+    ReactDOM.render(temp_owned, document.getElementById("upgrade_1_owned"))
+    consumed_total -= glass_price;
+    var consumednew = <h3 id="WaterBottlesCount">Water Bottles Produced: {consumed_total.toFixed(0)}</h3>
+    ReactDOM.render(consumednew, document.getElementById("counter"))
+    consumption_rate += 1;
+    glass_price = (glass_price * 1.02).toFixed(2);
+    var productionrate = <h3>Production Rate: {consumption_rate}</h3>
+    ReactDOM.render(productionrate, document.getElementById("multiplier"))
+    var temp_message = "PRICE: " + glass_price
+    ReactDOM.render(temp_message, document.getElementById("Upgrade1_Value"))
+
+  }
+}
+
+function new_bucket() {
+  if (consumed_total >= bucket_price) {
+    bucket_owned += 1;
+    var temp_own = "OWNED: " + bucket_owned;
+    ReactDOM.render(temp_own, document.getElementById("upgrade_2_owned"))
+    consumed_total -= bucket_price;
+    var consumednew = <h3 id="WaterBottlesCount">Water Bottles Produced: {consumed_total.toFixed(0)}</h3>
+    ReactDOM.render(consumednew, document.getElementById("counter"))
+    consumption_rate += 4;
+    bucket_price = (bucket_price * 1.13).toFixed(2);
+    var productionrate = <h3>Production Rate: {consumption_rate}</h3>
+    ReactDOM.render(productionrate, document.getElementById("multiplier"))
+    var temp_message = "PRICE: " + bucket_price
+    ReactDOM.render(temp_message, document.getElementById("Upgrade2_Value"))
+  }
+}
+
+function new_well() {
+  if (consumed_total >= well_price) {
+    well_owned += 1;
+    var temp_own = "OWNED: " + well_owned;
+    ReactDOM.render(temp_own, document.getElementById("upgrade_3_owned"))
+    consumed_total -= well_price;
+    var consumednew = <h3 id="WaterBottlesCount">Water Bottles Produced: {consumed_total.toFixed(0)}</h3>
+    ReactDOM.render(consumednew, document.getElementById("counter"))
+    consumption_rate += 20;
+    well_price = (well_price * 1.19).toFixed(2);
+    var productionrate = <h3>Production Rate: {consumption_rate}</h3>
+    ReactDOM.render(productionrate, document.getElementById("multiplier"))
+    var temp_message = "PRICE: " + well_price
+    ReactDOM.render(temp_message, document.getElementById("Upgrade3_Value"))
+  }
+
+}
+
+const mainheading = <h1>Drink some water</h1>
+const maindescription = <p>Hello there traveler. Sit down, drink some water, stay hydrated.
+  If you will need more - don't be shy. Click on the bottle and consume more.
+  Or contribute to making more by purchasing upgrades!
+</p>
+var counter = <h3 id="WaterBottlesCount">Water Bottles Produced: {consumed_total}</h3>
+var productionrate = <h3>Production Rate: {consumption_rate}</h3>
+const mybottleimage = <img class="bottle_img" src={bottle_image} alt="bottle" onClick={sip}/>
+const moreupgrade = <p>Purchase the following upgrades:</p>
+const upgrade1 = <img class="upgrade_img" src={glass_upgrade} alt="glass" onClick={new_glass}/>
+var glassprice = "PRICE: " + glass_price;
+const upgrade2 = <img class="upgrade_img" src={bucket_upgrade} alt="bucket" onClick={new_bucket}/>
+var bucketprices = "PRICE: " + bucket_price;
+const upgrade3 = <img class="upgrade_img" src={well_upgrade} alt="well" onClick={new_well}/>
+var wellprice = "PRICE: " + well_price;
+
+var upgrade_1_present = "OWNED: " + glass_owned;
+var upgrade_2_present = "OWNED: " + bucket_owned;
+var upgrade_3_present = "OWNED: " + well_owned;
+
+ReactDOM.render(mainheading, document.getElementById('mainheading'))
+ReactDOM.render(maindescription,document.getElementById("maindescription"))
+ReactDOM.render(counter, document.getElementById("counter"))
+ReactDOM.render(productionrate, document.getElementById("multiplier"))
+ReactDOM.render(mybottleimage, document.getElementById("bottle"))
+ReactDOM.render(moreupgrade, document.getElementById("upgrades_text"))
+
+ReactDOM.render(upgrade1, document.getElementById("upgrade_1"))
+ReactDOM.render(glassprice, document.getElementById("Upgrade1_Value"))
+ReactDOM.render(upgrade2, document.getElementById("upgrade_2"))
+ReactDOM.render(bucketprices, document.getElementById("Upgrade2_Value"))
+ReactDOM.render(upgrade3, document.getElementById("upgrade_3"))
+ReactDOM.render(wellprice, document.getElementById("Upgrade3_Value"))
+
+ReactDOM.render(upgrade_1_present, document.getElementById("upgrade_1_owned"))
+ReactDOM.render(upgrade_2_present, document.getElementById("upgrade_2_owned"))
+ReactDOM.render(upgrade_3_present, document.getElementById("upgrade_3_owned"))
+
+var pureTimer = setInterval(myTimer, 1000);
+function myTimer() {
+  consumed_total = consumed_total + consumption_rate
+  var counter = <h3 id="WaterBottlesCount">Water Bottles Produced: {consumed_total.toFixed(0)}</h3>
+  ReactDOM.render(counter, document.getElementById("counter"))
+}
